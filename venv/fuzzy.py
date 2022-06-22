f1572af9dc1e978842723f7245957c456499ecee
kris0454 <44412830+kris0454@users.noreply.github.com>
2022-06-22 11:53:41 +0200

    poprawka literówek

--- venv/fuzzy.py
+++ venv/fuzzy.py
@@ -3,12 +3,12 @@ import skfuzzy as fuzz
 import matplotlib.pyplot as plt
 
 #zmienne
-pogoda = np.arange(-10,10,1)
+pogoda = np.arange(-10,40,1)
 pompadzialanie = np.arange(-0.001,0.0003,0.001)
 #funkcje przynaleznosci
-pog_snieg = fuzz.trimf(pogoda,[8,10,10])
-pog_deszcz = fuzz.trimf(pogoda,[1,8,10])
-pog_slonce = fuzz.trapmf(pogoda,[-10,-10,1,8])
+pog_snieg = fuzz.trimf(pogoda,[-10,-5,0])
+pog_deszcz = fuzz.trimf(pogoda,[0,10,25])
+pog_slonce = fuzz.trimf(pogoda,[25,40,40])
 
 pom_działa = fuzz.trimf(pompadzialanie,[-0.001,-0.001,0])
 pom_doplyw = fuzz.trimf(pompadzialanie,[0.0001,0.0002,0.0003])
@@ -17,13 +17,13 @@ pom_off = fuzz.trimf(pompadzialanie,[-0.001,0,0.0001])
 fig, [ax1, ax2] = plt.subplots(nrows=2,ncols=1)
 
 ax1.plot(pogoda,pog_slonce,"r",pogoda,pog_deszcz,"m",pogoda,pog_snieg,"b")
-ax1.set_ylabel('Przynaleznosc')
-ax1.set_xlabel('Pogoda')
+ax1.set_ylabel('Przynależność')
+ax1.set_xlabel('Temperatura')
 ax1.set_ylim(-0.1,1.1)
 
 ax2.plot(pompadzialanie,pom_działa,'c',pompadzialanie,pom_doplyw,'m',pompadzialanie,pom_off,'ForestGreen')
 ax2.set_ylabel("Przynależność")
-ax2.set_xlabel("Działąnie pompy")
+ax2.set_xlabel("Działanie pompy")
 ax2.set_ylim(-0.1,1.1)
 
 R1 = fuzz.relation_product(pog_deszcz,pom_doplyw)
@@ -36,7 +36,7 @@ plt.imshow(R_combined)
 cbar = plt.colorbar()
 cbar.set_label('Przynależność')
 plt.yticks([i*10 for i in range(6)]),[str(i*10 -10) for i in range(6)]
-plt.ylabel('Pogoda')
+plt.ylabel('Temperatura')
 plt.xlabel('Pompa')
 
 plt.show()
